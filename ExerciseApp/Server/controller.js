d622eecb359c231672f082ffc003d4db098e930c
Matthew mcdonald <Matthewmcdonald703@gmail.com>
2018-10-18 02:49:24 -0400

    Choosing which other friend
    added a function for a list of friends that data can be shared with, does not currently share any data.

--- ExerciseApp/Server/controller.js
+++ ExerciseApp/Server/controller.js
@@ -1,19 +1,30 @@
 const express = require('express');
-const { EApp, User} = require('./model');
+const { EApp, User, Friends} = require('./model');
 
 var app = new EApp();
 
 const route = express.Router();
 
+// function to get currently does get
 route.get('/',function(req,res){
     res.send(app);
 })
 
-route.post('/Users',(req,res)=>{
+// post function for new user.
+route.post('/users',function(req,res){
 const user = new User(req.body.name)
 
 app.Users.push(user);
-res.send(users);
+res.send(user);
 });
+
+// post function for adding a new friend that data can be shared with.
+route.post('/shareWith',function(req,res){
+const share = new Friends(req.body.friend)
+
+app.CanBeSharedWithFirends.push(share);
+res.send(share);
+});
+
 
 module.exports = route;
--- ExerciseApp/Server/model.js
+++ ExerciseApp/Server/model.js
@@ -1,20 +1,26 @@
 
- 
+
+
 class EApp {
         constructor(){
          this.Users = [];
-         this.exersises = [];
-         this.friends = [];
+         this.CanBeSharedWithFirends = [];
         }
     }
 class User{
     constructor(name){
         this.name = name;
-        this.exersise = exersise;
-        this.friend = friend;
+        this.exersises = ["situp"];
+        this.friends = [];
+    }
+}
+
+class Friends{
+    constructor(friend){
+    this.friend = [friend];
     }
 }
 
 module.exports = {
-    User,EApp
+    User,EApp,Friends
 }
